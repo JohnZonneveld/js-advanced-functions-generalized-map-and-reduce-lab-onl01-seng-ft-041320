081d3a60d749852cbf384ae7a455a4a042d56350
raspimeteo <jjczon@gmail.com>
2020-07-31 21:45:25 -0500

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ function map(array, funct){
 }
 
 function reduce(arr, func, startPoint){
+    // if startPoint is given result starts with startPoint (value) otherwise with value stored in arr[0]
     let r = (!!startPoint) ? startPoint : arr[0]
+    // if startPoint is given start the array at position [0] otherwise [1]
     let i = (!!startPoint) ? 0 : 1
+    // i hs already set so in line below we 'skip' let i = part
     for (; i < arr.length; i++) {
       r = func(arr[i], r)
     }
