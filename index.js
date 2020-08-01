2edf9934c0155f0ac113bdb52e16639e3a8317f4
raspimeteo <jjczon@gmail.com>
2020-07-31 21:40:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, funct){
+    let r = []
+    for ( let i of array) {
+        r.push(funct(i))
+    }
+    return r
+}
+
+function reduce(arr, func, startPoint){
+    let r = (!!startPoint) ? startPoint : arr[0]
+    let i = (!!startPoint) ? 0 : 1
+    for (; i < arr.length; i++) {
+      r = func(arr[i], r)
+    }
+    return r;
+  }
